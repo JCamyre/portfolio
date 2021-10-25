c6290cabb85c4635c2f23ef9e6346a1c1ea9cc65
Joseph Camyre <jwcamry03@gmail.com>
2021-10-25 00:04:37 -0700

    Add profile pic and animate information when page is first loaded

---
+++ .next/static/webpack/webpack.54643d7f77e4aa864511.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "e3946518496a71c0b9ae"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay41NDY0M2Q3Zjc3ZTRhYTg2NDUxMS5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O1VBQUEscUNBQXFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcImUzOTQ2NTE4NDk2YTcxYzBiOWFlXCI7IH0iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ .next/static/webpack/webpack.9bcd004284713a549491.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "1435d1d5676acb0f8e10"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay45YmNkMDA0Mjg0NzEzYTU0OTQ5MS5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O1VBQUEscUNBQXFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjE0MzVkMWQ1Njc2YWNiMGY4ZTEwXCI7IH0iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
