af2b65d634d9eff4a1925032042841b266374118
Joseph Camyre <jwcamry03@gmail.com>
2021-12-20 23:20:28 -0800

    Fix import issues and eslint Next importing issues

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,19 @@
-import { ColorModeScript } from '@chakra-ui/react';
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import theme from '../lib/theme';
+import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import theme from '../lib/theme'
 
 // Does _document.js affect the base html document of the web application
 export default class Document extends NextDocument {
-    render() {
-        return (
-            <Html lang='en'>
-                <Head />
-            <body>
-                <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-                <Main />
-                <NextScript />
-            </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
