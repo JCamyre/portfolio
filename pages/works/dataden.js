ae4faad81f9a064bf60d04eeb5d496698fbd917b
Joseph Camyre <jwcamry03@gmail.com>
2022-03-16 22:54:43 -0700

    Testing

--- pages/works/dataden.js
+++ pages/works/dataden.js
@@ -1,25 +1,44 @@
-// Box,
-// SimpleGrid,
-// Center,
-// import {
-//   Container,
-//   Layout,
-//   Section,
-//   Paragraph,
-//   SimpleGrid
-// } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+// divider from '@chakra-ui/react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
 
-export default function work() {
+import thumbLunatrading from '../public/images/lunatradinglogo.png'
+import thumbDataDen from '../public/images/dataden.png'
+
+const DataDen = () => {
   return (
-    <div>yo</div>
-    // <Layout title="dataden">
-    //   <Container>
-    //     <SimpleGrid column={[1, 1, 2]} gap={6}>
-    //       <Section>
-    //         <Paragraph>yo</Paragraph>
-    //       </Section>
-    //     </SimpleGrid>
-    //   </Container>
-    // </Layout>
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={32} mb={4}>
+          DataDen
+        </Heading>
+        <SimpleGrid column={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem
+              id="lunatrading"
+              title="Lunatrading"
+              thumbnail={thumbLunatrading}
+              width={30}
+            >
+              An all-in-one trading info app
+            </WorkGridItem>
+          </Section>
+          <Section>
+            <WorkGridItem
+              id="dataden"
+              title="Data Den"
+              thumbnail={thumbDataDen}
+            >
+              Data aggregation and analytics using surveys, targetting college
+              demographics
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
+
+export default DataDen
