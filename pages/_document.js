b24c66a8f06b9d8e03dce630e7298af96bc26111
Joseph Camyre <jwcamry03@gmail.com>
2022-06-23 22:00:54 -0700

    Make favicon work for deployed application

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head>
-          <link
+          {/* <link
             rel="icon"
             type="image/png"
             sizes="32x32"
@@ -24,7 +24,7 @@ export default class Document extends NextDocument {
             rel="apple-touch-icon"
             sizes="180x180"
             href="images/apple-touch-icon.png"
-          />
+          /> */}
           <link rel="manifest" href="images/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="theme-color" content="#ffffff" />
