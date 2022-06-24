2e381558b2a0904c7b42883853a2a4d55722ea94
Joseph Camyre <jwcamry03@gmail.com>
2022-06-23 22:14:07 -0700

    test

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/main.js'
 import Fonts from '../components/fonts'
 import theme from '../lib/theme'
-import Head from 'Next/head'
+import Head from 'next/head'
 import { AnimatePresence } from 'framer-motion'
 // global styles
 import '../lib/main.css'
