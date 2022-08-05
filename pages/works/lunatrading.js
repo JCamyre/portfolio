d2b59fe891e7bf456525e6a27523471818a23ece
Joseph Camyre <jwcamry03@gmail.com>
2022-08-04 17:41:19 -0700

    Update Lunatrading page

--- pages/works/lunatrading.js
+++ pages/works/lunatrading.js
@@ -1,4 +1,4 @@
-import { Container, Heading, Image, Box } from '@chakra-ui/react'
+import { Container, Heading, Image, Box, Link } from '@chakra-ui/react'
 import Paragraph from '../../components/paragraph'
 // divider from '@chakra-ui/react'
 import Section from '../../components/section.js'
@@ -18,12 +18,30 @@ export default function Lunatrading(props) {
     <Container>
       <Section>
         <Heading>Lunatrading</Heading>
-        <Box align="center">
-          <Image src="/images/lunatradinglogo.png" alt="Lunatrading Logo" />
+        <Box align="center" mt="20px">
+          <a
+            href="https://github.com/JCamyre/Lunatrading"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="/images/lunatradinglogo.png"
+              h="100px"
+              alt="Lunatrading Logo"
+            />
+          </a>
         </Box>
       </Section>
       <Section>
-        <Paragraph>
+        <Paragraph style={{ lineHeight: '32px' }}>
+          My favorite project ever is my Stock Trading Information Web App
+          called Lunatrading! I started working on this project out of laziness:
+          I was looking at over 10 different websites to get all the information
+          I needed to decide whether to buy a stock or not! I used React on the
+          frontend, and Python and Django for the backend. I also created my own
+          Python package from scratch that enabled coders to get any information
+          for a stock that they needed.
+          <br />
           ● Built stock information presenter SPA using React and Django REST
           Framework
           <br />
