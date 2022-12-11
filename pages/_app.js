084891e1c862edbca36db2ae1143b39376cd0b3c
JCamyre <jwcamry03@gmail.com>
2022-12-10 21:32:43 -0800

    Fix Google analytics and add it to .env

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ const Website = ({ Component, pageProps, router }) => {
     <>
       <Script
         strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_INSIGHTS_KEY}`}
       />
       <Script
         id="google-analytics"
@@ -23,7 +23,7 @@ const Website = ({ Component, pageProps, router }) => {
           window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-XXXXXXX', {
+              gtag('config', ${process.env.GOOGLE_INSIGHTS_KEY}, {
               page_path: window.location.pathname,
               });
               `
