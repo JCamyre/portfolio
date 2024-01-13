6dbeafd1ed9a6fcfa22f2ada52482c78860004c8
JCamyre <jwcamry03@gmail.com>
2024-01-12 21:46:55 -0800

    Actually delete lunar trading

--- components/works.js
+++ components/works.js
@@ -2,16 +2,12 @@ import { Container, Heading, SimpleGrid, Stack } from '@chakra-ui/react'
 import Section from '../components/section.js'
 import { WorkGridItem } from '../components/grid-item'
 import Layout from '../components/layouts/article'
-import thumbLunatrading from '../public/images/lunatradinglogo.png'
 import thumbAcm from '../public/images/acm-logo.png'
 import thumbViasat from '../public/images/viasat-logo.png'
 import thumbChips from '../public/images/chips-logo.png'
-import thumbDIPD from '../public/images/DIPD-logo.jpg'
-import thumbBAI from '../public/images/bai-logo.png';
 import thumbDevX from '../public/images/devx-logo.jpg';
 import thumbCapitalOne from '../public/images/capital-one.png'
 import thumbFork from "../public/images/fork-logo.jpg"
-import thumbLunarTrading from "../public/images/lunar-trading-logo.png"
 
 const Works = () => {
   return (
@@ -40,15 +36,6 @@ const Works = () => {
                 Enhance phone authentication system, reduce the 25% of the 300 million customer support calls a year falsely requiring further verification, improving millions of users&apos; experience and saving over $2 million in Operational Expenses.
               </WorkGridItem>
             </Section>
-            <Section>
-              <WorkGridItem
-                id="lunartrading"
-                title="Lunar Trading"
-                thumbnail={thumbLunarTrading}
-              >
-                Democratize investing and empower college students by leveraging AI and ML.
-              </WorkGridItem>
-          </Section>
             <Section>
               <WorkGridItem
                 id="viasat"
